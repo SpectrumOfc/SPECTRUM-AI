c785d3778155dc490a0a0f161efd8725dfc92b73
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-10-12 20:12:42 -1100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('SPECTRUM AI', {
+say('\nSPECTRUM\nAI', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
