bfb0d50dd5f05287ca98ae239741854e6689e7d6
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-11-11 10:21:09 -1100

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,39 +1,45 @@
+import fg from 'api-dylux';
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
-import fg from 'api-dylux'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-let limit = 350 
+let limit = 350;
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-        if (!args || !args[0]) throw `✳️ Ejemplo :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
-    if (!args[0].match(/youtu/gi)) throw `❎ Verifica que el link de YouTube`
-         let chat = global.db.data.chats[m.chat]
-         m.react(rwait) 
-        try {
-                let q = args[1] || '360p'
-                let v = args[0]
-                const yt = await youtubedl(v).catch(async () => await youtubedlv2(v))
-                const dl_url = await yt.video[q].download()
-                const title = await yt.title
-                const size = await yt.video[q].fileSizeH 
+    if (!args || !args[0]) throw `✳️ Ejemplo :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`;
+    if (!args[0].match(/youtu/gi)) throw `❎ Verifica que el link de YouTube`;
 
-       if (size.split('MB')[0] >= limit) return m.reply(` ≡  *FG YTDL*\n\n▢ *⚖️Peso* : ${size}\n▢ *🎞️Calidad* : ${q}\n\n▢ _El archivo supera el límite de descarga_ *+${limit} MB*`)    
-          conn.sendFile(m.chat, dl_url, title + '.mp4', `
+    let chat = global.db.data.chats[m.chat];
+    m.react('⏳'); // Emoji de espera
+
+    try {
+        let q = args[1] || '360p';
+        let v = args[0];
+        const yt = await youtubedl(v).catch(async () => await youtubedlv2(v));
+        const dl_url = await yt.video[q].download();
+        const title = await yt.title;
+        const size = await yt.video[q].fileSizeH;
+
+        if (size.split('MB')[0] >= limit) {
+            return m.reply(` ≡  *FG YTDL*\n\n▢ *⚖️Peso* : ${size}\n▢ *🎞️Calidad* : ${q}\n\n▢ _El archivo supera el límite de descarga_ *+${limit} MB*`);
+        }
+
+        conn.sendFile(m.chat, dl_url, title + '.mp4', `
  ≡  *FG YTDL*
   
 ▢ *📌Título* : ${title}
 ▢ *📟 Ext* : mp4
 ▢ *🎞️Calidad* : ${q}
 ▢ *⚖️Peso* : ${size}
-`.trim(), m, false, { asDocument: chat.useDocument })
-                m.react(done) 
+`.trim(), m, false, { asDocument: chat.useDocument });
+        
+        m.react('✅'); // Emoji para indicar que ha terminado
 
-        } catch {        
-       m.reply(`✳️ Error al descargar el video intenta con otro`) 
-        } 
+    } catch {        
+        m.reply(`✳️ Error al descargar el video intenta con otro`); 
+    }
+};
 
-}
-handler.help = ['ytmp4 <link yt>']
-handler.tags = ['dl'] 
-handler.command = ['ytmp4', 'fgmp4']
-handler.diamond = true
+handler.help = ['ytmp4 <link yt>'];
+handler.tags = ['dl'];
+handler.command = ['ytmp4', 'fgmp4'];
+handler.diamond = true;
 
-export default handler
+export default handler;
