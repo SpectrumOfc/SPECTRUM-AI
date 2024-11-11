716daff2e06d3631806c2613d42168a3ddda278f
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-11-10 18:39:36 -1100

    Update and rename group-tag.js to grupo|tag.js

--- plugins/grupo|tag.js
+++ plugins/grupo|tag.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants}) => {
 let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-if (!m.quoted) return m.reply(`🍭 Responde a un mensaje.`)
+if (!m.quoted) return m.reply(`🚨 𝐕𝐮𝐞𝐥𝐯𝐞 𝐚 𝐮𝐬𝐚𝐫 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐩𝐞𝐫𝐨 𝐫𝐞𝐬𝐩𝐨𝐧𝐝𝐢𝐞𝐧𝐝𝐨 𝐚 𝐮𝐧 𝐦𝐞𝐧𝐬𝐚𝐣𝐞.`)
 conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 handler.help = ['tag']
