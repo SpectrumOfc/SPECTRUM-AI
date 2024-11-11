0ba9fd27e9cac80f457af4ffb2aff31a5a3510c8
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-11-11 12:22:26 -1100

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,13 +1,11 @@
 function handler(m) {
-  const data = global.owner.filter(([id, , isCreator]) => id && isCreator);
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m);
-  
-  // Envía un mensaje de texto diciendo "hola" después de enviar los contactos
-  this.sendMessage(m.chat, 'hola', m);
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+
 }
 
-handler.help = ['owner'];
-handler.tags = ['main'];
-handler.command = ['owner', 'creator', 'creador', 'dueño'];
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
 
-export default handler;
+export default handler
