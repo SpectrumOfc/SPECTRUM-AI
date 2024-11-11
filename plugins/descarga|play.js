631ca971442cea9eddcba62fd8a1f6b9bc228367
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-11-11 05:09:13 -1100

    Update descarga|play.js

--- plugins/descarga|play.js
+++ plugins/descarga|play.js
@@ -70,7 +70,7 @@ const cukaDownloader = {
 let dataos = await cukaDownloader.youtube(tes.url, "mp3")
 console.log(dataos)
 let { title, thumbnail, quality, downloadUrl } = dataos
-  m.reply(`𝐏𝐋𝐀𝐘 | 𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐀𝐈\n\n𝐓𝐈𝐓𝐔𝐋𝐎: *${title} (${quality})*\n𝐔𝐑𝐋: *${tes.url}*`)
+  m.reply(`♫♪♩·.¸𝐏𝐋𝐀𝐘 | 𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐀𝐈¸.·♩♪♫\n\n𝐓𝐈𝐓𝐔𝐋𝐎: ${title} (${quality})\n𝐔𝐑𝐋: ${tes.url}`)
       const doc = {
       audio: { url: downloadUrl },
       mimetype: 'audio/mp4',
