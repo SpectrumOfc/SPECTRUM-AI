17f7430c67a407deb0fdf0e7db7dcfcf29f0a36a
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-11-12 18:29:12 -1100

    Update downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,47 +1,25 @@
-import { search, download } from 'aptoide-scraper';
-
-const handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
-  if (!text) {
-    m.reply(`❱❱ 𝙄 𝙉 𝙁 𝙊 𝙍 𝙈 𝘼 𝘾 𝙄 𝙊 𝙉 ❰❰\n\n🔮 𝙁𝙤𝙧𝙢𝙖𝙩𝙤 𝙞𝙣𝙘𝙤𝙧𝙧𝙚𝙘𝙩𝙤\n\n» 𝙐𝙨𝙚 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤:\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: .𝘢𝘱𝘵𝘰𝘪𝘥𝘦 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱`);
-    return;
-  }
-
-  try {
-    const searchA = await search(text);
+let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+if (!args[0]) throw `🍭 ingrese el nombre de la app que desea buscar`
+let res = await fetch(`https://api.dorratz.com/v2/apk-dl?text=${args[0]}`);
+let result = await res.json();
+let { name, size, lastUpdate, icon } = result;
+let URL = result.dllink
+let packe = result.package
+let texto = `  ゲ◜៹ Aptoide Scraper ៹◞ゲ*\n
+     *☍ Nombre ∙*  ${name} 📩
+     *☍ peso ∙*  ${size} ⚖️
+     *☍ package ∙*  ${packe} 📦
+     *☍ Publicado ∙* ${lastUpdate} 🗓️
     
-    // Verifica si hay resultados
-    if (!searchA || searchA.length === 0) {
-      throw new Error("No se encontraron resultados");
-    }
+🍭 Enviando archivo por favor espere suelo ser lenta..`
+await conn.sendFile(m.chat, icon, name + '.jpg', texto, m)
 
-    const appData = await download(searchA[0].id);
+await conn.sendMessage(m.chat, { document: { url: URL }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: ''}, { quoted: m });
+}
+handler.tags = ['descargas']
+handler.help = ['apkmod']
+handler.command = /^(aptoidedl)$/i
+handler.register = false
+handler.estrellas = 1
 
-    const response = `📲 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙎 𝘼𝙋𝙏𝙊𝙄𝘿𝙀 📲\n\n🔥 𝙉𝙊𝙈𝘽𝙍𝙀: ${appData.name}\n🚀 𝙋𝘼𝘾𝙆𝘼𝙂𝙀: ${appData.package}\n⏳ 𝙐𝙇𝙏𝙄𝙈𝘼 𝘼𝘾𝙏..: ${appData.lastup}\n🕋 𝙏𝘼𝙈𝘼Ñ𝙊: ${appData.size}`;
-    
-    await conn.sendMessage(m.chat, {
-      image: { url: appData.icon },
-      caption: response
-    }, { quoted: m });
-
-    if (appData.size.includes('GB') || parseFloat(appData.size.replace(' MB', '')) > 999) {
-      await conn.sendMessage(m.chat, {
-        text: '❱❱ 𝙄 𝙉 𝙁 𝙊 𝙍 𝙈 𝘼 𝘾 𝙄 𝙊 𝙉 ❰❰\n\n🔮 𝘌𝘭 𝘢𝘳𝘤𝘩𝘪𝘷𝘰 𝘦𝘴 𝘥𝘦𝘮𝘢𝘴𝘪𝘢𝘥𝘰 𝘱𝘦𝘴𝘢𝘥𝘰 𝘱𝘰𝘳 𝘭𝘰 𝘲𝘶𝘦 𝘯𝘰 𝘴𝘦 𝘦𝘯𝘷𝘪𝘢𝘳𝘢.'
-      }, { quoted: m });
-      return;
-    }
-
-    await conn.sendMessage(m.chat, {
-      document: { url: appData.dllink },
-      mimetype: 'application/vnd.android.package-archive',
-      fileName: `${appData.name}.apk`,
-      caption: null
-    }, { quoted: m });
-
-  } catch (error) {
-    console.error(error);
-    m.reply(`❱❱ 𝙄 𝙉 𝙁 𝙊 𝙍 𝙈 𝘼 𝘾 𝙄 𝙊 𝙉 ❰❰\n\n🔮 Error, no se encontraron resultados para su búsqueda.`);
-  }
-};
-
-handler.command = /^(apk|modapk|dapk2|aptoide|aptoidedl)$/i;
-export default handler;
+export default handler
