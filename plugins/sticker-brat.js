7e17042eb276160dbd0742ad5fdace03a0d4759b
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-11-12 18:18:12 -1100

    Update sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,36 +1,35 @@
-import fs from 'fs'
-  import WSF from "wa-sticker-formatter";
-var handler = async (m, {
-    conn, 
-    args, 
-    text, 
-    usedPrefix, 
-    command
-}) => {
-    let ps = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.quoted && m.quoted.caption ? m.quoted.caption : m.quoted && m.quoted.description ? m.quoted.description : ''
-    if (!ps) throw m.reply(`*• Ejemplo :* ${usedPrefix + command} *[texto]*`)
+import fs from 'fs';
+import WSF from "wa-sticker-formatter";
 
-    let res = `https://mxmxk-helper.hf.space/brat?text=${ps}`
+var handler = async (m, { conn, args, text, usedPrefix, command }) => {
+    let ps = text 
+        ? text 
+        : m.quoted && (m.quoted.text || m.quoted.caption || m.quoted.description)
+        ? m.quoted.text || m.quoted.caption || m.quoted.description
+        : '';
+
+    if (!ps) throw m.reply(`*• Ejemplo :* ${usedPrefix + command} *[texto]*`);
+
+    let res = `https://mxmxk-helper.hf.space/brat?text=${ps}`;
 
     try {
-async function sticker(img, url, packname, author, categories = [""]) {
+        async function sticker(img, url, packname, author, categories = [""]) {
+            const stickerMetadata = {
+                type: "full",
+                pack: packname,
+                author,
+                categories,
+            };
+            return await new WSF.Sticker(img ? img : url, stickerMetadata).build();
+        }
 
-  const stickerMetadata = {
-    type: "full",
-    pack: packname,
-    author,
-    categories,
-  };
-  return await new WSF.Sticker(img ? img : url, stickerMetadata).build();
-}
-
-        var stikerp = await sticker(res, { ps })
-        await conn.sendFile(m.chat, stikerp, 'atmin.webp', '', m)
+        var stikerp = await sticker(null, res, "PackName", "AuthorName");  // Asegúrate de poner "PackName" y "AuthorName" o cambiarlo
+        await conn.sendFile(m.chat, stikerp, 'sticker.webp', '', m);
     } catch (e) {
-        console.log(e)
-        await m.reply(eror)
+        console.log(e);
+        await m.reply("Ocurrió un error al generar el sticker");
     }
-}
+};
 
 handler.help = ['brat'];
 handler.tags = ['sticker'];
